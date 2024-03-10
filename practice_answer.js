6834206cfc51c20126c9e586f785cbc11293face
tukky-14 <bigoonage0104@gmail.com>
2024-03-10 13:12:52 +0900

    JavaScriptの練習問題とその回答を追加

---
+++ practice_answer.js
@@ -0,0 +1,76 @@
+// ### 問題1: 変数の宣言と代入
+// JavaScriptで変数を宣言し、文字列「Hello, World!」を代入して、コンソールに表示させてください。
+// const helloWorld = 'Hello, World!';
+// console.log(helloWorld);
+
+// ### 問題2: 算術演算子
+// 2つの数値変数を宣言し、それらの和、差、積、商をコンソールに表示させてください。
+// const num1 = 10;
+// const num2 = 5;
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+
+// ### 問題3: 型の確認
+// 異なる型（文字列、数値、真偽値）の変数をそれぞれ宣言し、それぞれの型をコンソールに表示させてください。
+// const str = 'Hello, World!';
+// const num = 1;
+// const bool = true;
+// console.log(typeof str);
+// console.log(typeof num);
+// console.log(typeof bool);
+
+// ### 問題4: 条件式
+// 数値を1つ格納した変数が偶数か奇数かを判定し、結果をコンソールに表示させてください。
+// const num = 10;
+// if (num % 2 === 0) {
+//     console.log('偶数です');
+// } else {
+//     console.log('奇数です');
+// }
+
+// ### 問題5: 配列とループ
+// 5つの異なるフルーツの名前を格納した配列を作成し、それらを一つずつコンソールに表示させてください。
+// const fruits = ['apple', 'banana', 'orange', 'grape', 'melon'];
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+
+// ### 問題6: 関数の定義と呼び出し
+// 2つの数値を引数として受け取り、それらの和を返す関数を定義して、使用してみてください。
+// function addNum(num1, num2) {
+//     return num1 + num2;
+// }
+// console.log(addNum(10, 5));
+
+// ### 問題7: オブジェクト
+// 名前、年齢、趣味をプロパティとしてもつオブジェクトを作成し、それぞれのプロパティをコンソールに表示させてください。
+// const person = {
+//     name: 'taro',
+//     age: 20,
+//     hobby: 'reading',
+// };
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.hobby);
+
+// ### 問題8: 条件式（応用）
+// 成績を数値で格納した変数があります。90以上なら「優秀」、70以上90未満なら「良好」、50以上70未満なら「合格」、それ以下なら「不合格」とコンソールに表示させてください。
+// const score = 80;
+// if (score >= 90) {
+//     console.log('優秀');
+// } else if (score >= 70) {
+//     console.log('良好');
+// } else if (score >= 50) {
+//     console.log('合格');
+// } else {
+//     console.log('不合格');
+// }
+
+// ### 問題9: 関数（応用）
+// 3つの数値を引数として受け取り、最大値を返す関数を定義して使用してみてください。
+// function maxNum(num1, num2, num3) {
+//     return Math.max(num1, num2, num3);
+// }
+// console.log(maxNum(10, 20, 30));
